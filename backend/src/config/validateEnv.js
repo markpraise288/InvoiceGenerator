60725d9e7d749ccfcbe5ea6168de5b73f2c1ad54
markpraise288 <markpraise288@gmail.com>
2026-04-28 11:56:25 +0200

    Rename MONGODB_URI to MONGODB_URL and clean up
    Updated environment variable names and removed unused variables.

--- backend/src/config/validateEnv.js
+++ backend/src/config/validateEnv.js
@@ -2,13 +2,9 @@ const Joi = require('joi');
 
 const envVarsSchema = Joi.object({
   PORT: Joi.number().default(3000),
-  MONGODB_URI: Joi.string().required().description('Mongo DB url'),
+  MONGODB_URL: Joi.string().required().description('Mongo DB url'),
   JWT_SECRET: Joi.string().required().description('JWT secret key'),
   CORS_ORIGIN: Joi.string().default('*'),
-  REDIS_HOST: Joi.string().default('localhost'),
-  REDIS_PORT: Joi.number().default(6379),
-  PAYPAL_CLIENT_ID: Joi.string().required().description('PayPal Client ID'),
-  PAYPAL_SECRET: Joi.string().required().description('PayPal Secret'),
 }).unknown().required();
 
 const { error, value: envVars } = envVarsSchema.validate(process.env);
@@ -19,11 +15,7 @@ if (error) {
 
 module.exports = {
   PORT: envVars.PORT,
-  MONGODB_URI: envVars.MONGODB_URI,
+  MONGODB_URL: envVars.MONGODB_URL,
   JWT_SECRET: envVars.JWT_SECRET,
   CORS_ORIGIN: envVars.CORS_ORIGIN,
-  REDIS_HOST: envVars.REDIS_HOST,
-  REDIS_PORT: envVars.REDIS_PORT,
-  PAYPAL_CLIENT_ID: envVars.PAYPAL_CLIENT_ID,
-  PAYPAL_SECRET: envVars.PAYPAL_SECRET,
 };
