1f124a178dab54b49f0d9a7c8e802ad872617328
Praise Malunga <markpraise288@gmail.com>
2026-05-08 08:55:38 +0200

    added attachments to email service

--- backend/src/infrastructure/email/email.service.js
+++ backend/src/infrastructure/email/email.service.js
@@ -3,13 +3,14 @@ const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async ({ to, subject, html }) => {
+const sendEmail = async ({ to, subject, html, attachments }) => {
   try {
     const msg = {
       to,
       from: process.env.EMAIL_FROM, // must be verified in SendGrid
       subject,
       html,
+      attachments,
     };
 
     await sgMail.send(msg);
